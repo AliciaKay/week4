1471dced316f4df2fdf4e56193183bef3dc19d78
Alicia Kay <aliciakaywilking@gmail.com>
2017-08-07 22:18:59 -0500

    Add files via upload

--- 3_objectLiteralCreate.js
+++ 3_objectLiteralCreate.js
@@ -1,30 +1,38 @@
 // Create books with object literal syntax
-function threeBooks(title, author, pageCount) {
+function threeBooks() {
   // Create 3 book objects with object literal syntax. Books should have properties for title, author, and pageCount.
   // Write code here:
-  this.title = title;
-  this.author = author;
-  this.pageCount = pageCount;
-}
 
-var book1 = new threeBooks("Yes Please", "Amy Poehler", 329);
+  var book1 = {
+      title: "Yes Please",
+      author: "Amy Poehler",
+      pageCount: 329
+    };
 
-var book2 = new threeBooks("Harriet the Spy", "Louise Fitzhugh", 330);
+  var book2 = {
+      title: "Harriet the Spy",
+      author: "Louise Fitzhugh",
+      pageCount: 330
+    };
 
-var book3 = new threeBooks("Franny and Zooey", "J.D. Salinger", 202);
+  var book3 = {
+      title: "Franny and Zooey",
+      author: "J.D. Salinger",
+      pageCount: 202
+    };
 
   // Add a property of haveRead to each book object you created. This should be set to true or false.
   // Write code here:
-threeBooks.prototype.haveRead = boolean;
-book1.haveRead = true;
-book2.haveRead = true;
-book3.haveRead = false;
+  book1.haveRead = true;
+  book2.haveRead = true;
+  book3.haveRead = false;
+
   // Store these objects in an array called books.
   // Write code here:
-var books = Array.from(threeBooks);
+  var books = [book1, book2, book3];
 
   // return this array
   return books;
 }
 // call threeBooks and console log the return
-console.log(threeBooks);
+console.log(threeBooks());
